6b08b5ed6d40a8cd9699261abdd9823cd7c78acc
Bob Main <bobmain49@gmail.com>
2017-07-09 16:26:41 -0400

    madness . . .

--- component.js
+++ component.js
@@ -71,8 +71,8 @@ class Component {
 
 var componentsStoredGlobally = []
 var polyFillIncluded = false
-var hash = window.location.hash.split('#')[1]
-var allPages = { hash: window.location.hash.split('#')[1] }
+// var hash = window.location.hash.split('#')[1]
+var pageKey = { hash: window.location.hash.split('#')[1] }
 
 /* () () () () () () () () ()   Page  Handling  () () () () () () () () () () */
 
@@ -80,33 +80,68 @@ document.onreadystatechange = ()=>{
   if (document.readyState === 'complete') {
     let elms = document.querySelectorAll('[pageName]')
     // try and combine some of these loops here. you've got FOUR!
-    for (elm of elms) allPages[elm.getAttribute('pageName')] = []
+    for (elm of elms) pageKey[elm.getAttribute('pageName')] = []
     for (elm of elms) {
       if (elm.querySelector('[pageName]')) {
         for (c of elm.querySelectorAll('[pageName]')) {
-          allPages[elm.getAttribute('pageName')].push(c.getAttribute('pageName'))
-          allPages[c.getAttribute('pageName')].push(elm.getAttribute('pageName'))
+          pageKey[c.getAttribute('pageName')].push(elm.getAttribute('pageName'))
         }
       }
     }
-    console.log(allPages)
+    let activePage = ''
     for (elm of elms) {
       const name = elm.getAttribute('pageName')
-      if (elm.hasAttribute('activePage')) pageSet(elm.tagName, name, true)
+      if (elm.hasAttribute('activePage')) {
+        pageSet(elm.tagName, name, true)
+        activePage = name
+      }
     } // ! need to be seperate loops for #overides to properly clean up.
     for (elm of elms) {
       const name = elm.getAttribute('pageName')
-      if (name === hash) pageSet(elm.tagName, name, true)
+      if (name === pageKey.hash) {
+        pageSet(elm.tagName, name, true)
+        activePage = name
+      }
     }
+    pageKey.hash = activePage
+    if (activePage) window.location.href = '#' + activePage
+
+    for (page of pageKey[activePage]) {
+      pageKey[page].push(activePage)
+    }
+    console.log('activePage: ', activePage)
+    console.log('pageKey: ',pageKey)
+
   }
 }
 
 pageSet = (group, switchTo, browserEvent)=>{
+  // can we clean up ... etc         pageKey[pageKey[pageKey.hash]].splice(pageKey[pageKey[pageKey.hash]].indexOf(pageKey.hash), 1)
   if (!browserEvent) {
-    hash = switchTo
-    window.location.href = window.location.hash.split('#')[0] + '#' + hash
-  }
+    console.log('start pageKey.hash, switchTo:', pageKey.hash, switchTo)
 
+    if (pageKey[switchTo].length > 0) {
+      if (pageKey.hash) { // Do we still need this for edge-case?
+        // is the new page in the old one?
+        console.log('--1', pageKey[pageKey[pageKey.hash]])
+        console.log('--2', pageKey[switchTo])
+        if (pageKey[pageKey[pageKey.hash]]) {
+          if (pageKey[pageKey[pageKey.hash]].includes(pageKey.hash)) {
+            pageKey[pageKey[pageKey.hash]].splice(pageKey[pageKey[pageKey.hash]].indexOf(pageKey.hash), 1)
+          }
+          pageKey.hash = switchTo
+          if (pageKey[pageKey[pageKey.hash]]) { // redudent???
+            pageKey[pageKey[pageKey.hash]].push(pageKey.hash)
+            // can we only create keys when there's dependencies.. think about cleaning that up.
+          }
+        }
+
+
+      }
+    }
+    pageKey.hash = switchTo
+    window.location.href = window.location.hash.split('#')[0] + '#' + pageKey.hash
+  }
   let pages = document.getElementsByTagName(group)
   for (var i = 0; i < pages.length; i++) { // iOS does not like (i of arr) here... for some reason
      if (switchTo === pages[i].getAttribute('pageName')) {
@@ -120,9 +155,11 @@ pageSet = (group, switchTo, browserEvent)=>{
 window.onhashchange = function() {
   hash = window.location.hash.split('#')[1]
   let page = document.querySelector('[pageName="'+hash+'"')
-  console.log(page)
   // you should really just rewrite this whole thing using the page-group object...
+  // so we need to pair up the state of the active children, so when we return we can remember who was active
+
   pageSet(page.tagName, hash, true)
+  console.log('pageKey: ',pageKey)
 
   // if (hash) {
   //   pageSet(page.tagName, hash, true)
